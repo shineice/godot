a484e8a21dd423ccec9078d3b9c85646dea03041
zoe <test@com.tw>
2018-05-27 14:35:47 +0800

    levelselection but2

--- 06-Grid-based movement/LevelSelection.gd
+++ 06-Grid-based movement/LevelSelection.gd
@@ -10,7 +10,7 @@ func _ready():
 	var bt1=get_node("buttonMonkeySaver")
 	bt1.connect("pressed", self, "switchLevel", ["monkeySaver"])  #[物件, 動作值]
 	var bt2=get_node("buttonFisherman")
-	bt1.connect("pressed", self, "switchLevel", ["buttonFisherman"])  #[物件, 動作值]
+	bt2.connect("pressed", self, "switchLevel", ["buttonFisherman"])  #[物件, 動作值]
 	pass
 
 func switchLevel(level):
