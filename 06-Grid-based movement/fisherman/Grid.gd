a76f0c72dc6b83beb97375c226339d1e87d49c29
lendle <lendle_tseng@seed.net.tw>
2018-06-04 14:33:20 +0800

    prepare the release/1.0 branch

--- 06-Grid-based movement/fisherman/Grid.gd
+++ 06-Grid-based movement/fisherman/Grid.gd
@@ -3,32 +3,38 @@ extends TileMap
 
 enum ENTITY_TYPES {PLAYER, OBSTACLE, COLLECTIBLE}
 
-var tile_size = Vector2(50,50)#get_cell_size()
+var tile_size = Vector2(65,65)#get_cell_size()
 var half_tile_size = tile_size / 2
-var grid_size = Vector2(17,7)#Vector2(16, 16)
+var grid_size = Vector2(13,7)#Vector2(16, 16)
+
 
 var grid = []
-onready var Obstacle = preload("res://Obstacle.tscn")
-onready var Obstacle_1 = preload("res://Obstacle_1.tscn")
-onready var Obstacle_2 = preload("res://Obstacle_2.tscn")
-onready var Obstacle_3 = preload("res://Obstacle_3.tscn")
-onready var Obstacle_4 = preload("res://Obstacle_4.tscn")
-onready var Player = preload("res://Player.tscn")
+var grid_inst=[]
+onready var Obstacle_1 = preload("res://fisherman/sheep.tscn")
+onready var Obstacle_2 = preload("res://fisherman/wolf.tscn")
+onready var Player = preload("res://fisherman/Player.tscn")
+
 
 #define the map
 onready var map={
-	[15,5]:"Obstacle", #鍋
-	[15,1]:"Obstacle_1", #刀
-	[10,1]:"Obstacle_3",
-	[10,5]:"Obstacle_4",
-	[3,3]:"Obstacle_2" #loop
+	[3,3]:"Obstacle_1",  
+	[9,2]:"Obstacle_2",
+	[9,4]:"Obstacle_2",
+	[9,6]:"Obstacle_2", 
+	[11,1]:"Obstacle_2", 
+	[11,3]:"Obstacle_2",
+	[11,5]:"Obstacle_2"
 }
 
 func _ready():
+	var global=get_node("/root/global");
+	global.point = "fisherman_1"
 	for x in range(grid_size.x):
 		grid.append([])
+		grid_inst.append([])
 		for y in range(grid_size.y):
 			grid[x].append(null)
+			grid_inst[x].append(null)
 
 	# Player
 	var new_player = Player.instance()
@@ -47,9 +53,8 @@ func _ready():
 	#				placed = true
 
 	for pos in positions:
-		var new_obstacle = Obstacle.instance()
+		var new_obstacle = Obstacle_1.instance()
 		new_obstacle.set_pos(map_to_world(pos) + half_tile_size)
-		#new_obstacle.set_pos(pos*50-tile_size/2)
 		grid[pos.x][pos.y] = new_obstacle.get_name()
 		add_child(new_obstacle)
 
@@ -58,18 +63,13 @@ func _ready():
 		var pos=Vector2(entry[0], entry[1])
 		positions.append(pos)
 		var new_obstacle=null
-		if(map[entry]=="Obstacle"):
-			new_obstacle=Obstacle.instance()
-		elif(map[entry]=="Obstacle_1"):
+		if(map[entry]=="Obstacle_1"):
 			new_obstacle=Obstacle_1.instance()
 		elif(map[entry]=="Obstacle_2"):
 			new_obstacle=Obstacle_2.instance()
-		elif(map[entry]=="Obstacle_3"):
-			new_obstacle=Obstacle_3.instance()
-		elif(map[entry]=="Obstacle_4"):
-			new_obstacle=Obstacle_4.instance()
 		new_obstacle.set_pos(map_to_world(pos) + half_tile_size)
 		grid[pos.x][pos.y] = new_obstacle.get_name()
+		grid_inst[pos.x][pos.y]=new_obstacle
 		add_child(new_obstacle)
 
 func get_cell_content(pos=Vector2()):
@@ -89,9 +89,13 @@ func update_child_pos(new_pos, direction, type):
 	var grid_pos = world_to_map(new_pos)
 	print(grid_pos)
 	grid[grid_pos.x][grid_pos.y] = null
-	
 	var new_grid_pos = grid_pos + direction
 	grid[new_grid_pos.x][new_grid_pos.y] = type
-	
 	var target_pos = map_to_world(new_grid_pos) + half_tile_size
 	return target_pos
+	
+
+func is_goal(pos):
+	return pos.x==9 and pos.y==0
+	
+
--- 06-Grid-based movement/fisherman/Player.gd
+++ 06-Grid-based movement/fisherman/Player.gd
@@ -13,11 +13,13 @@ var is_moving = false
 
 var type
 var grid
+var game
 
-
+var expanded_steps=[]
 
 func _ready():
 	grid = get_parent()
+	game=grid.get_parent()
 	type = grid.PLAYER
 	set_fixed_process(true)
 
@@ -27,25 +29,48 @@ func _fixed_process(delta):
 	var global=get_node("/root/global");
 	if(!global.running):
 		return;
-	
 	direction = Vector2()
 	speed = 0
 #以陣列放置player動作,要先叫陣列
 #陣列.讀取陣列中第一個動作,看是甚麼
-	if global.steps[global.index]=="up": 
-		direction.y = -1
-	elif global.steps[global.index]=="down":
-		direction.y = 1
-	elif global.steps[global.index]=="left":
-		direction.x = -1
-	elif global.steps[global.index]=="right":
-		direction.x = 1
+	if global.index<global.expandedSteps.size():
+		if global.expandedSteps[global.index]=="up": 
+			direction.y = -1
+		elif global.expandedSteps[global.index]=="down":
+			direction.y = 1
+		elif global.expandedSteps[global.index]=="left":
+			direction.x = -1
+		elif global.expandedSteps[global.index]=="right":
+			direction.x = 1
 	
-	if not is_moving and direction != Vector2():
+	if not is_moving and global.gameStatus=="normal":
 		target_direction = direction.normalized()
-		#if grid.is_cell_vacant(get_pos(), direction):
-		target_pos = grid.update_child_pos(get_pos(), direction, type)
-		is_moving = true
+		if grid.is_cell_vacant(get_pos(), direction):
+			target_pos = grid.update_child_pos(get_pos(), direction, type)
+			if target_pos==null:
+				is_moving=false
+			else:
+				is_moving = true
+		else:
+			is_moving=false
+			if grid.is_goal(grid.world_to_map(get_pos())+direction):
+				global.gameStatus="success"
+			else:
+				global.gameStatus="fail"
+		
+	elif global.gameStatus=="success":
+		set_fixed_process(false)
+		global.running=false
+		global.complete="Y"
+		game.upload_game_result()
+		game.show_success()
+	
+	elif global.gameStatus=="fail":
+		global.running=false
+		global.complete="N"
+		game.upload_game_result()
+		game.show_fail()
+		
 	elif is_moving:
 		speed = MAX_SPEED
 		velocity = speed * target_direction * delta
@@ -57,19 +82,8 @@ func _fixed_process(delta):
 		if move_distance > distance_to_target:
 			velocity = target_direction * distance_to_target
 			is_moving = false
-			#不刪除功能start
-			#global.steps.pop_front();
 			global.index = global.index+1
-			if(global.steps.size()==global.index):  
+			if(global.expandedSteps.size()==global.index and 1==0):  
 				global.running=false;
-			#End
-				
-				global.gamepoint(global.mapid,global.studentid,"point2",String(OS.get_unix_time()),String(global.steps.size()),global.complete)
-				var s={"value":global.list}.to_json()
-				global.gamestatus(s);
-				print("json"+s.percent_encode())
-				#for i in range(global.index):
-					#print(global.list[i][0],global.mapid,global.list[i][1],global.list[i][2],global.list[i][3])
-					#global.gamestatus(global.list[i][0],global.mapid,global.list[i][1],global.list[i][2],global.list[i][3])
 
 		move(velocity)
