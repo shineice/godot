2b7501cda1b433cb91494e943a18838b5e7928df
lendle <lendle_tseng@seed.net.tw>
2018-06-25 09:49:15 +0800

    adjust export settings and save to local disk

--- 06-Grid-based movement/Global.gd
+++ 06-Grid-based movement/Global.gd
@@ -14,6 +14,7 @@ var currentGame="maze"
 var currentLevel=1
 
 var gameStatus="idle" #normal/fail/success
+var SAVE_LOCALLY=1
 
 func reset():
 	steps=[]
@@ -23,64 +24,91 @@ func reset():
 	gameStatus="normal"
 
 func gamepoint(mapid,stuid,gamepoint,datetime,blocknum,complete):
-    var err=0
-    var http = HTTPClient.new() 
-    var err = http.connect("imsofa.rocks",8080) 
-    assert(err==OK) # Make sure connection was OK
-    while( http.get_status()==HTTPClient.STATUS_CONNECTING or http.get_status()==HTTPClient.STATUS_RESOLVING):
-        http.poll()
-        print("Connecting..")
-        OS.delay_msec(100)
+	if SAVE_LOCALLY==1:
+		var dir=Directory.new()
+		var savePath="user://gamepoint.save"
+		if(dir.dir_exists("/sdcard")):
+			savePath="/sdcard/gamepoint.save"
+		var savegame = File.new()
+		if(savegame.file_exists(savePath)):
+			savegame.open(savePath, File.READ_WRITE)
+			savegame.seek_end();
+		else:
+			savegame.open(savePath, File.WRITE)
+		savegame.store_line("mapid="+mapid+"&stuid="+stuid+"&gamepoint="+gamepoint+"&datetime="+datetime+"&blocknum="+blocknum+"&complete="+complete)
+		savegame.close()
+		return
+	var err=0
+	var http = HTTPClient.new() 
+	var err = http.connect("imsofa.rocks",8080) 
+	assert(err==OK) # Make sure connection was OK
+	while( http.get_status()==HTTPClient.STATUS_CONNECTING or http.get_status()==HTTPClient.STATUS_RESOLVING):
+		http.poll()
+		print("Connecting..")
+		OS.delay_msec(100)
 
-    assert( http.get_status() == HTTPClient.STATUS_CONNECTED ) 
+	assert( http.get_status() == HTTPClient.STATUS_CONNECTED ) 
 
-    var headers=[
-       "User-Agent: Pirulo/1.0 (Godot)",
-        "Accept: */*"
-    ]
-    var req="mapid="+mapid+"&stuid="+stuid+"&gamepoint="+gamepoint+"&datetime="+datetime+"&blocknum="+blocknum+"&complete="+complete
-    err = http.request(HTTPClient.METHOD_GET,"/GameProgramLearningServer/GamePointService?"+req,headers) # Request a page from the site (this one was chunked..)
+	var headers=[
+	   "User-Agent: Pirulo/1.0 (Godot)",
+		"Accept: */*"
+	]
+	var req="mapid="+mapid+"&stuid="+stuid+"&gamepoint="+gamepoint+"&datetime="+datetime+"&blocknum="+blocknum+"&complete="+complete
+	err = http.request(HTTPClient.METHOD_GET,"/GameProgramLearningServer/GamePointService?"+req,headers) # Request a page from the site (this one was chunked..)
 
-    assert( err == OK ) # Make sure all is OK
+	assert( err == OK ) # Make sure all is OK
 
-    while (http.get_status() == HTTPClient.STATUS_REQUESTING):
-        # Keep polling until the request is going on
-        http.poll()
-        print("Requesting.."+String(http.get_status()))
-        OS.delay_msec(100)
+	while (http.get_status() == HTTPClient.STATUS_REQUESTING):
+		# Keep polling until the request is going on
+		http.poll()
+		print("Requesting.."+String(http.get_status()))
+		OS.delay_msec(100)
 
-    assert( http.get_status() == HTTPClient.STATUS_BODY or http.get_status() == HTTPClient.STATUS_CONNECTED ) # Make sure request finished well.
-    print("response? ",http.has_response()) 
+	assert( http.get_status() == HTTPClient.STATUS_BODY or http.get_status() == HTTPClient.STATUS_CONNECTED ) # Make sure request finished well.
+	print("response? ",http.has_response()) 
 
 func gamestatus(req):
-    var err=0
-    var http = HTTPClient.new() 
-    var err = http.connect("imsofa.rocks",8080) 
-    assert(err==OK) # Make sure connection was OK
-    while( http.get_status()==HTTPClient.STATUS_CONNECTING or http.get_status()==HTTPClient.STATUS_RESOLVING):
-        http.poll()
-        print("Connecting..")
-        OS.delay_msec(100)
+	if SAVE_LOCALLY==1:
+		var dir=Directory.new()
+		var savePath="user://gamestatus.save"
+		if(dir.dir_exists("/sdcard")):
+			savePath="/sdcard/gamestatus.save"
+		var savegame = File.new()
+		if(savegame.file_exists(savePath)):
+			savegame.open(savePath, File.READ_WRITE)
+			savegame.seek_end();
+		else:
+			savegame.open(savePath, File.WRITE)
+		savegame.store_line(req)
+		savegame.close()
+		return
+	var err=0
+	var http = HTTPClient.new() 
+	var err = http.connect("imsofa.rocks",8080) 
+	assert(err==OK) # Make sure connection was OK
+	while( http.get_status()==HTTPClient.STATUS_CONNECTING or http.get_status()==HTTPClient.STATUS_RESOLVING):
+		http.poll()
+		print("Connecting..")
+		OS.delay_msec(100)
 
-    assert( http.get_status() == HTTPClient.STATUS_CONNECTED ) 
+	assert( http.get_status() == HTTPClient.STATUS_CONNECTED ) 
 
-    var headers=[
-       "User-Agent: Pirulo/1.0 (Godot)",
-        "Accept: */*"
-    ]
-    err = http.request(HTTPClient.METHOD_POST,"/GameProgramLearningServer/GameStatusService?req="+req.percent_encode(),headers) # Request a page from the site (this one was chunked..)
+	var headers=[
+	   "User-Agent: Pirulo/1.0 (Godot)",
+		"Accept: */*"
+	]
+	err = http.request(HTTPClient.METHOD_POST,"/GameProgramLearningServer/GameStatusService?req="+req.percent_encode(),headers) # Request a page from the site (this one was chunked..)
 
-    assert( err == OK ) # Make sure all is OK
+	assert( err == OK ) # Make sure all is OK
 
-    while (http.get_status() == HTTPClient.STATUS_REQUESTING):
-        # Keep polling until the request is going on
-        http.poll()
-        print("Requesting.."+String(http.get_status()))
-        OS.delay_msec(100)
+	while (http.get_status() == HTTPClient.STATUS_REQUESTING):
+		# Keep polling until the request is going on
+		http.poll()
+		print("Requesting.."+String(http.get_status()))
+		OS.delay_msec(100)
 
-    assert( http.get_status() == HTTPClient.STATUS_BODY or http.get_status() == HTTPClient.STATUS_CONNECTED ) # Make sure request finished well.
-    print("response? ",http.has_response()) 
-
+	assert( http.get_status() == HTTPClient.STATUS_BODY or http.get_status() == HTTPClient.STATUS_CONNECTED ) # Make sure request finished well.
+	print("response? ",http.has_response()) 
 
 func back2SceneSwitcher():
 	get_tree().change_scene("res://LevelSelection.tscn")
